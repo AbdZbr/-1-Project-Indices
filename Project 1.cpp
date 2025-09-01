b4aa70b1443c6063dd2220121b8f4f2dc99d996d
AbdZbr <abdullahzubair.mohd@cuw.edu>
2025-08-31 23:28:14 -0500

    Add files via upload

---
+++ Project 1.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> numMap;
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+            if (numMap.find(complement) != numMap.end()) {
+                return {numMap[complement], i};
+            }
+            numMap[nums[i]] = i;
+        }
+        return {};
+    }
+};
+
+int main() {
+    Solution solution;
+
+    // Example 1
+    vector<int> nums1 = {2, 7, 11, 15};
+    int target1 = 9;
+    vector<int> result1 = solution.twoSum(nums1, target1);
+    cout << "Example 1 Output: [" << result1[0] << ", " << result1[1] << "]" << endl;
+
+    // Example 2
+    vector<int> nums2 = {3, 2, 4};
+    int target2 = 6;
+    vector<int> result2 = solution.twoSum(nums2, target2);
+    cout << "Example 2 Output: [" << result2[0] << ", " << result2[1] << "]" << endl;
+
+    // Example 3
+    vector<int> nums3 = {3, 3};
+    int target3 = 6;
+    vector<int> result3 = solution.twoSum(nums3, target3);
+    cout << "Example 3 Output: [" << result3[0] << ", " << result3[1] << "]" << endl;
+
+    return 0;
+}
